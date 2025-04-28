ca438043a2c468e5e93ff3a9872eae785807d802
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2025-04-28 18:27:05 +0200

    imprint en and de

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
-import { ViewportScroller } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
+
 
 @Component({
   selector: 'app-imprint', 
   standalone: true,
   imports: [
     TranslateModule,
-    RouterLink
+    RouterLink, 
+    CommonModule
   ],
   templateUrl: './imprint.component.html', 
   styleUrl: './imprint.component.scss' 
 })
 export class ImprintComponent implements OnInit {
   
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor(
+    private viewportScroller: ViewportScroller,
+    private translateService: TranslateService
+  ) {}
   
   ngOnInit(): void {
     this.viewportScroller.scrollToPosition([0, 0]);
+    
+    console.log('Current lang:', this.translateService.currentLang);
+    console.log('Available translation keys:', this.translateService.instant('IMPRINT'));
+  }
+  
+  forceLanguage(lang: string): void {
+    this.translateService.use(lang);
+    console.log('Switched to language:', lang);
   }
 }
